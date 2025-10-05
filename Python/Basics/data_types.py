4a5a05a9b6c3e8ceb332ce9bdb74d5d93e8731bd
sidduyadav00 <siddarthasirnam@gmail.com>
2025-10-06 01:19:55 +0530

    Create data_types.py

---
+++ Python/Basics/data_types.py
@@ -0,0 +1,12 @@
+# Python/Basics/data_types.py
+# Shows different basic data types in Python
+
+x = 10          # Integer
+y = 3.14        # Float
+z = "Hello"     # String
+is_active = True  # Boolean
+
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
+print(type(is_active), is_active)
