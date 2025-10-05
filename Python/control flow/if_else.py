b5b7cb200110c70da473f88bde44ac4bf894c2d6
sidduyadav00 <siddarthasirnam@gmail.com>
2025-10-06 01:26:56 +0530

    Create if_else.py

---
+++ Python/control flow/if_else.py
@@ -0,0 +1,11 @@
+# Python/Control_Flow/if_else.py
+# Basic if-else statement
+
+number = 7
+
+if number > 0:
+    print("Positive number")
+elif number == 0:
+    print("Zero")
+else:
+    print("Negative number")
