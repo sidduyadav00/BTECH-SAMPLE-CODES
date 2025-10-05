73e55c534e6b063753a4ca3edf0f01ad7499ad42
sidduyadav00 <siddarthasirnam@gmail.com>
2025-10-06 01:18:54 +0530

    Create variables.py

---
+++ Python/Basics/variables.py
@@ -0,0 +1,10 @@
+# Python/Basics/variables.py
+# Demonstrates how to create and print variables
+
+name = "HBTech"
+age = 2025 - 2000  # Calculate age
+pi = 3.14159
+
+print("Name:", name)
+print("Age:", age)
+print("Pi value:", pi)
